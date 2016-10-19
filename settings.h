a363718e31f9f78a31af30977a0fad71a7676a29
jan <jan-dufek@users.noreply.github.com>
2016-10-19 14:33:23 -0500

    Reorganization of the settings file

--- settings.h
+++ settings.h
@@ -12,9 +12,11 @@ using namespace cv;
 // USB
 // Sometimes the external USB camera is on 0 and sometimes on 1
 VideoCapture video_capture(1);
+int saturation_min = 120;
+int value_min = 10;
 
 // If USB device is Inogeni, uncomment this to set correct FPS
-#define INOGENI
+//#define INOGENI
 
 // Screen mirroring application from DJI tablet
 //VideoCapture video_capture("rtsp://10.201.147.238:5000/screen");
@@ -27,15 +29,23 @@ VideoCapture video_capture(1);
 
 // Lake Bryan AI Robotic class field test 2016 03 31
 //VideoCapture video_capture("input/2016_03_31_lake_bryan.mp4");
+//int saturation_min = 52;
+//int value_min = 10;
 
 // Fort Bend floods 2016 04 26
 //VideoCapture video_capture("input/2016_04_26_fort_bend.mp4");
+//int saturation_min = 120;
+//int value_min = 100;
 
 // Lake Bryan AI Robotics class final 2016 05 10
 //VideoCapture video_capture("input/2016_05_10_lake_bryan.mov");
+//int saturation_min = 10;
+//int value_min = 10;
 
 // Lab 2016 07 05
 //VideoCapture video_capture("input/2016_07_05_lab.avi");
+//int saturation_min = 130;
+//int value_min = 10;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Algorithm Variable parameters
@@ -48,29 +58,19 @@ int hue_1_max = 10;
 // Hue 2 range for thresholding
 int hue_2_min = 160;
 //int hue_2_min = 180; // Works well for lab
-
 int hue_2_max = 180;
 
 // Saturation range for thresholding
-//int saturation_min = 52; // Works well for Lake Bryan 1
-//int saturation_min = 120; // Works well for Fort Bend
-int saturation_min = 10; // Works well for Lake Bryan 2
-//int saturation_min = 130; // Works well for lab
-
 int saturation_max = 255;
 
 // Value range for thresholding
-//int value_min = 10; // Works well for Lake Bryan 1
-//int value_min = 100; // Works well for Fort Bend
-int value_min = 10; // Works well for Lake Bryan 2
-
 int value_max = 255;
 
 // Gaussian blur kernel size
 int blur_kernel_size = 21;
 
 // When the centroid of EMILY gets closer or equal to this number of pixels, it will consider target to be reached
-int target_radius = 10;
+int target_radius = 30;
 
 // Value of the proportional parameter of PID
 int proportional = 10;
