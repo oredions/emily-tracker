f929c69d28914602aaaf4f05f72cf96d85c5cf87
jan <jan-dufek@users.noreply.github.com>
2016-11-12 21:44:26 -0600

    EMILY will gradually slow down before reaching the target.

--- control.cpp
+++ control.cpp
@@ -1,4 +1,5 @@
 #include "control.h"
+#include "settings.h"
 #include <math.h> 
 #include <stdio.h>
 #include <iostream>
@@ -7,9 +8,35 @@ using namespace std;
 
 #define PI 3.14159265
 
+// Turning throttle
 const double turning_throttle = 0.3;
+
+// Cruising throttle
 const double cruising_throttle = 0.6;
-double diff;
+
+// Angle difference
+double angle_difference;
+
+/**
+ * Gets throttle limited by max_throttle determined by distance to target.
+ * 
+ * @param max_throttle maximum allowable throttle
+ * @param distance_to_target distance to target
+ * @return 
+ */
+double get_throttle(double max_throttle, double distance_to_target) {
+    
+    // We are closer that slowing threshold
+    if (distance_to_target < slowing_distance * target_radius) {
+        
+        return max_throttle * (distance_to_target / ((double) slowing_distance * target_radius));
+        
+    } else {
+        
+        return max_throttle;
+        
+    }
+}
 
 commands get_control_commands(int xe_i, int ye_i, double theta, int xv_i, int yv_i) {
 
@@ -17,34 +44,40 @@ commands get_control_commands(int xe_i, int ye_i, double theta, int xv_i, int yv
     double ye = (double) ye_i;
     double xv = (double) xv_i;
     double yv = (double) yv_i;
-    
+
     double kp = proportional / 1000.0;
 
     double target_vector;
-    target_vector = atan2(yv - ye, xv - xe)* 180 / PI;
+    target_vector = atan2(yv - ye, xv - xe) * 180 / PI;
+    
     commands current_commands;
     current_commands.throttle = 0;
     current_commands.rudder = 0;
-    double dist = sqrt(pow(xe - xv, 2) + pow(ye - yv, 2));
+    
+    double distance_to_target = sqrt(pow(xe - xv, 2) + pow(ye - yv, 2));
 
     // Save distance to target
-    current_commands.distance_to_target = dist;
-    
-    if (dist < target_radius) { // already reached target
+    current_commands.distance_to_target = distance_to_target;
+
+    if (distance_to_target < target_radius) { // already reached target
         cout << "Reached the target." << endl;
         target_reached = true;
         return current_commands;
     } else {
         if (fabs(target_vector - theta) < 180) { //normal case
-            
+
             // Save error angle to target
             current_commands.angle_error_to_target = target_vector - theta;
-            
+
             if (fabs(target_vector - theta) < 30) { //PID mode
-                current_commands.throttle = cruising_throttle;
+
+                current_commands.throttle = get_throttle(cruising_throttle, distance_to_target);
                 current_commands.rudder = kp * (target_vector - theta);
+                
             } else { //turning mode
-                current_commands.throttle = turning_throttle;
+                
+                current_commands.throttle = get_throttle(turning_throttle, distance_to_target);
+                
                 if (target_vector > theta) {
                     current_commands.rudder = 1.0; //turn left is positive
                 } else {
@@ -54,20 +87,20 @@ commands get_control_commands(int xe_i, int ye_i, double theta, int xv_i, int yv
         }
         if (fabs(target_vector - theta) >= 180) {// consider turning in other direction
             if (target_vector > theta) {
-                diff = (target_vector - theta) - 360;
+                angle_difference = (target_vector - theta) - 360;
             } else {
-                diff = (target_vector - theta) + 360;
+                angle_difference = (target_vector - theta) + 360;
             }
-            
+
             // Save error angle to target
-            current_commands.angle_error_to_target = diff;
-            
-            if (fabs(diff) < 30) { //PID mode
-                current_commands.throttle = cruising_throttle;
-                current_commands.rudder = kp * diff;
+            current_commands.angle_error_to_target = angle_difference;
+
+            if (fabs(angle_difference) < 30) { //PID mode
+                current_commands.throttle = get_throttle(cruising_throttle, distance_to_target);
+                current_commands.rudder = kp * angle_difference;
             } else { //turning mode
-                current_commands.throttle = turning_throttle;
-                if (diff > 0) {
+                current_commands.throttle = get_throttle(turning_throttle, distance_to_target);
+                if (angle_difference > 0) {
                     current_commands.rudder = 1.0; //turn left is positive
                 } else {
                     current_commands.rudder = -1.0; //turn right is negative
@@ -75,11 +108,14 @@ commands get_control_commands(int xe_i, int ye_i, double theta, int xv_i, int yv
             }
         }
     }
+
     if (current_commands.rudder > 1.0) {
         current_commands.rudder = 1.0;
     }
-    if (current_commands.rudder<-1.0) {
+
+    if (current_commands.rudder < -1.0) {
         current_commands.rudder = -1.0;
     }
+
     return current_commands;
 }
